61001859abec69aacddf469f31454def391f9875
su-voz-a-diario <ricardogz777@gmail.com>
2026-08-10 21:18:02 -0600

    Ajusta color e icono del botón Continuar Leyendo

--- js/app.js
+++ js/app.js
@@ -9343,7 +9343,7 @@ renderBible: function() {
                         data-action="open-bible-continue"
                     >
                         <div class="bible-action-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" /><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>
                         </div>
                         <div class="bible-action-content">
                             <div class="bible-action-title">
